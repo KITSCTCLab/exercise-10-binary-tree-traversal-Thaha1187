2ed38b606c1c63bdff53dd38cc9648cae668e572
Thaha1187 <110010500+Thaha1187@users.noreply.github.com>
2022-10-11 20:57:52 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,65 +1,66 @@
 class BinaryTreeNode:
-    
-    def __init__(self, data):
+     def __init__(self, data)
         self.data = data
         self.left_child = None
         self.right_child = None
-
-
+   
+   
 def insert(root, new_value) -> BinaryTreeNode:
-      """If binary search tree is empty, make a new node, declare it as root and return the root.
+    """If binary search tree is empty, make a new node, declare it as root and return the root.
         If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
         If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
         Finally, return the root.
         """
-   # Write your code here
-        if self.data:
-            if data<self.data:
-                if self.left is Null:
-                    self.left = BinaryTreeNode(data)
-                else:
-                    self.left.append(data)
-            else if(data>self.data):
-                if self.right is Nill:
-                    self.right = BinaryTreeNode(data)
-                 else:
-                    self.right.append(data)
-         else:
-            self.data = data
-
+    # Write your code here
+    if (root == None):
+        root = BinaryTreeNode(new_value)
+        return root
+    else:
+        if root.data > new_value:
+            if root.left_child is None:
+                new_node = BinaryTreeNode(new_value)
+                root.left_child = new_node
+            else:
+                insert(root.left_child,new_value)
+               
+        else:
+            if root.right_child is None:
+                new_node = BinaryTreeNode(new_value)
+                root.right_child = new_node
+            else:
+                insert(root.right_child,new_value)
 
 def inorder(root) -> None:
-        # Write your code here
-        def inorderTraversal(self, root):
-            res = []
-            if root:
-                res = self.inorderTraversal(root.left)
-                res.append(root.data)
-                res = res + self.inorderTraversal(root.right)
-        return res
+    """
+    to print in the order
+    left mid right
+    """
+    if root:
+        inorder(root.left_child)
+        print(root.data, end = " ")
+        inorder(root.right_child)
 
 
- def preorder(root) -> None:
-        # Write your code here
-        def PreorderTraversal(self, root):
-            res = []
-            if root:
-                res.append(root.data)
-                res = res + self.PreorderTraversal(root.left)
-                res = res + self.PreorderTraversal(root.right)
-            return res
+def preorder(root) -> None:
+    """
+    to print in the order
+    mid left right
+    """
+    if root:
+        print(root.data, end = " ")
+        preorder(root.left_child)
+        preorder(root.right_child)
 
 
 def postorder(root) -> None:
-        # Write your code here
-      def PostorderTraversal(self, root):
-            res = []
-            if root:
-                res = self.PostorderTraversal(root.left)
-                res = res + self.PostorderTraversal(root.right)
-                res.append(root.data)
-            return res
-
+    """
+    to print in the order
+    left right left
+    """
+    if root:
+        postorder(root.left_child)
+        postorder(root.right_child)
+        print(root.data, end = " ")
 
 # Do not change the following code
 input_data = input()
