71b4f71237ca783860566222566c8cca3f04494a
Thaha1187 <110010500+Thaha1187@users.noreply.github.com>
2022-10-11 12:07:06 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,18 +12,52 @@ def insert(root, new_value) -> BinaryTreeNode:
         Finally, return the root.
         """
     # Write your code here
+    if self.data:
+        if data<self.data:
+            if self.left is Null:
+                self.left = BinaryTreeNode(data)
+            else:
+                self.left.append(data)
+        else if(data>self.data):
+            if self.right is Nill:
+                self.right = BinaryTreeNode(data)
+             else:
+                self.right.append(data)
+     else:
+        self.data = data
 
 
 def inorder(root) -> None:
     # Write your code here
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.data)
+            res = res + self.inorderTraversal(root.right)
+    return res
 
 
 def preorder(root) -> None:
     # Write your code here
+    def PreorderTraversal(self, root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.PreorderTraversal(root.left)
+            res = res + self.PreorderTraversal(root.right)
+        return res
 
 
 def postorder(root) -> None:
     # Write your code here
+    def PostorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.PostorderTraversal(root.left)
+            res = res + self.PostorderTraversal(root.right)
+            res.append(root.data)
+        return res
 
 
 # Do not change the following code
